ffba1612e7c2b7bb56680200e8aefeb96dc0a7bf
kulakowka <kulakowka@gmail.com>
2016-02-06 17:20:01 +0300

    update projects

--- controllers/mainpage/index.js
+++ controllers/mainpage/index.js
@@ -22,17 +22,17 @@ module.exports = function index (req, res, next) {
 }
 
 function loadProjects () {
-  return Project.find().populate('platform,language').limit(10).exec()
+  return Project.find().limit(6).exec()
 }
 
 function loadLanguages () {
-  return Language.find().limit(15).exec()
+  return Language.find().limit(9).exec()
 }
 
 function loadPlatforms () {
-  return Platform.find().limit(15).exec()
+  return Platform.find().limit(9).exec()
 }
 
 function loadTutorials () {
-  return Tutorial.find().limit(10).exec()
+  return Tutorial.find().limit(5).exec()
 }
