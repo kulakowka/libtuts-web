62e17f6a265a949246d7dee2883b899fb002880f
kulakowka <kulakowka@gmail.com>
2016-02-06 06:04:08 +0300

    active links

--- middlewares/viewHelpers.js
+++ middlewares/viewHelpers.js
@@ -5,6 +5,7 @@ const manifest = require('../public/rev-manifest')
 module.exports = function viewHelpersMiddleware (req, res, next) {
   res.locals.currentUser = req.session.user
   res.locals.env = req.app.get('env')
+  res.locals.url = req.url
   res.locals.assetPath = (path) => {
     if (req.app.get('env') !== 'production') return `/${path}`
     return `/${manifest[path] || path}`
