222e04e10a7258bd9382ec3127a05133264ba49b
kulakowka <kulakowka@gmail.com>
2016-02-08 12:33:36 +0300

    numeral

--- middlewares/viewHelpers.js
+++ middlewares/viewHelpers.js
@@ -2,12 +2,14 @@
 
 const manifest = require('../public/rev-manifest')
 const moment = require('moment')
+const numeral = require('numeral')
 
 module.exports = function viewHelpersMiddleware (req, res, next) {
   res.locals.currentUser = req.session.user
   res.locals.env = req.app.get('env')
   res.locals.url = req.url
   res.locals.moment = moment
+  res.locals.numeral = numeral
   res.locals.assetPath = (path) => {
     if (req.app.get('env') !== 'production') return `/${path}`
     return `/${manifest[path] || path}`
