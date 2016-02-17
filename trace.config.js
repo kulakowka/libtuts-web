a9de09bc78ae8ee7039b7ec536800268326bff81
kulakowka <kulakowka@gmail.com>
2016-02-17 03:40:24 +0300

    remove trace

--- trace.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  serviceName: 'WEB',
-  apiKey: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjU2YzFkZDNlYzRmNDE5MTEwMGYzNWYwYyIsImlhdCI6MTQ1NTU0NTY2Mn0.eem2a7b1ys_G8ai_jgz46cSXMUIcEs3eHnsStu_Fwsk'
-}
