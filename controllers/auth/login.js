cea61fecc363d0db5169c83e0c99e463c694aa8a
kulakowka <kulakowka@gmail.com>
2016-02-17 03:18:10 +0300

    auth flow complete

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -4,15 +4,15 @@ const API = require('../../utils/api')
 
 // POST /auth/login
 module.exports = function *(req, res, next) {
-  let user = yield loadUser(req.body)
+  let user = yield loginUser(req.body)
   if (!user) return res.json({error: {msg: 'Incorrect username or password.'}})
   if (!user.errors) req.session.user = user
   res.json(user)
 }
 
-function loadUser (body) {
-  return API.model('user').findOne({
-    username: body.identifier,
+function loginUser (body) {
+  return API.model('user').action('login', 'post').form({
+    identifier: body.identifier,
     password: body.password
   }).exec()
 }
--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -7,17 +7,16 @@ const MailerAPI = require('../../utils/mailerApi')
 module.exports = function *(req, res, next) {
   let user = yield createUser(req.body)
   if (user.errors) return res.json(user)
-  let token = yield createVerificationToken(user.id)
+  let verificationToken = yield createVerificationToken(user.id)
+  // здесь мы отправим запрос в мейлер и попросим его отправить юзеру письмо
+  let result = yield MailerAPI.sendVerifyEmail(user, verificationToken.token)
   req.session.user = user
   res.json(user)
-
-  // здесь мы отправим запрос в мейлер и попросим его отправить юзеру письмо
-  let result = yield MailerAPI.sendVerifyEmail(user, token)
   console.log('sendVerifyEmail result:', result)
 }
 
 function createVerificationToken (user) {
-  return API.model('verificationToken').create({user}).exec().then(verificationToken => verificationToken.token)
+  return API.model('verificationToken').create({user}).exec()
 }
 
 function createUser (body) {
